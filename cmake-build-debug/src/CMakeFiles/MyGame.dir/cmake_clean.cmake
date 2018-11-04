ba02d9143aef90cb5de7f2af917b7acf89c5439a
Dimtk26 <36993458+Dimtk26@users.noreply.github.com>
2018-11-05 02:24:16 +0600

    Add files via upload

---
+++ cmake-build-debug/src/CMakeFiles/MyGame.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyGame.dir/game/PlayerMovementSystem.cpp.obj"
+  "CMakeFiles/MyGame.dir/game/Tile.cpp.obj"
+  "CMakeFiles/MyGame.dir/game/WallSystem.cpp.obj"
+  "CMakeFiles/MyGame.dir/game/main.cpp.obj"
+  "MyGame.pdb"
+  "MyGame.exe"
+  "MyGame.exe.manifest"
+  "libMyGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
